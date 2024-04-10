f8faa7e9257e5799b2a3a27345c20d36f8c10a31
Adriana Sukhanova <adriana_sukhanova@epam.com>
2024-04-10 19:33:30 +0300

    Add constants file

---
+++ js/constants.js
@@ -0,0 +1,18 @@
+/** @format */
+
+export const colorsList = [
+  'red',
+  'greenyellow',
+  'yellow',
+  'black',
+  'royalblue',
+  'violet',
+  'purple',
+  'pink',
+  'lightsalmon',
+];
+
+export const resultCSSClasses = {
+  resultRow: 'result__row',
+  resultColorBox: 'result__color-box',
+};
